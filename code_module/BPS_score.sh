459d5cf2f6f6e566ff98cedc5d29940bfe211901
ChangwenPu <124153052+CHWEPUUU@users.noreply.github.com>
2026-04-28 14:07:43 +0800

    Update BPS_score.sh

--- code_module/BPS_score.sh
+++ code_module/BPS_score.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 # set -euo pipefail
 
-source /home/data/cq/.local/miniconda3/etc/profile.d/conda.sh 
-conda activate scBPS 
-
 gwas=${1:-Dutch207}
-zscore_file=/home/data/share/module/scRNA/scBPS/GutMicrobiome_zscores/gene_zscore_${gwas}.txt
+zscore_file=./GutMicrobiome_zscores/gene_zscore_${gwas}.txt
 
 if [[ ! -f "${zscore_file}" ]]; then
     echo "[ERROR] zscore file not found: ${zscore_file}" >&2
     exit 1
 fi
 
-snakemake -s /home/data/share/module/scRNA/scBPS/code_module/scBPS.smk \
+snakemake -s ./code_module/scBPS.smk \
     --cores 32 \
     --config zscore_file="${zscore_file}" \
              scfile=./output/sc_adata.h5ad \
